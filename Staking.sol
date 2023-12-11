acc92008c767a36b46039a41c52600c59b2437f6
ESTHER ISRAEL OKON <105334293+Smartbabbe@users.noreply.github.com>
2023-12-11 11:43:26 -0800

    Update Staking.sol

--- Staking.sol
+++ Staking.sol
@@ -10,17 +10,20 @@ contract Staking {
     IERC20 public token;
     address public owner;
     uint256 public startTime;
+    uint256 public duration;
 
 
      mapping (address => uint256) public stakedAmount;
 
-     constructor (address _token) {
+     constructor (address _token, uint256 _stakeStartTime, uint256 _stakeduration) {
         owner = msg.sender;
         token = IERC20(_token);
-        startTime = block.timestamp;
+        startTime = _stakeStartTime;
+        duration = _stakeduration;
      }
 
      function stake(uint256 _amount) external {
+        require(block.timestamp >= startTime, "Wait for staking to start");
         require (_amount > 0, "Can't donate zero value");
 
         token.transferFrom(msg.sender, address(this), _amount);
@@ -31,7 +34,8 @@ contract Staking {
     }
 
     function withdraw(uint256 _amount) external  {
-        require(msg.sender == owner);
+        require(block.timestamp >= duration, "Wait for staking to end");
+        require(msg.sender == owner, "You are not the owner");
         require(_amount > 0, "Amount must be greater than 0");
         require(stakedAmount[msg.sender] >= _amount, "Insufficient balance");
 
@@ -46,4 +50,3 @@ contract Staking {
     }
 
 }
-
